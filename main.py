6da535391e8a6575a47e0a15b9a9363583281015
KevinMenden <kevin.menden@t-online.de>
2019-03-25 15:27:14 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,148 @@
+"""
+Main file for using CDN
+"""
+
+import argparse
+from model.cdn import CDN
+from model.functions import *
+import tensorflow as tf
+
+if __name__=="__main__":
+
+    """
+    Argument parsing section
+    """
+    # Argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str, help="Which mode. One of [ train | predict | train_predict | process ]", default="train")
+    parser.add_argument("--data", type=str, help="Path to dataset")
+    parser.add_argument("--model", type=str, help="Model directory", default="/home/kevin/jobs")
+    parser.add_argument("--name", type=str, help="Model name", default="cdn_model")
+    parser.add_argument("--batch_size", type=int, help="Batch size. Default: 128", default=128)
+    parser.add_argument("--learning_rate", "-l", type=float, help="Learning rate. Default: 0.0001", default=0.0001)
+    parser.add_argument("--steps", type=int, help="Number of steps to use for training", default=1000)
+    parser.add_argument("--processed_path", type=str, help="Out path for processed data", default="./processed.h5ad")
+    parser.add_argument("--scaling", type=str, help="Scaling option. Default: log_min_max", default="log_min_max")
+    parser.add_argument("--out", type=str, help="Path to store output in. Default: cwd", default="./")
+    parser.add_argument("--training_data", type=str, help="Path to training dataset.", default=None)
+    parser.add_argument("--outname", type=str, help="Name of the prediction output file. Default: cdn_predictions.txt", default="cdn_predictions.txt")
+    # Parse args
+    args = parser.parse_args()
+    mode = args.mode
+    data_path = args.data
+    model_dir = args.model
+    model_name = args.name
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    num_steps = args.steps
+    processed_path = args.processed_path
+    scaling_option = args.scaling
+    out_dir = args.out
+    training_data = args.training_data
+    out_name = args.outname
+
+
+    """
+    TRAINING ONLY
+    In training only mode, a CDN Model is trained only on the training data
+    For later predictions, it might be the case that the set of training features does not
+    entirely overlap
+    """
+
+    # Training mode
+    if mode == "train":
+        with tf.Session() as sess:
+            cdn = CDN(sess=sess,
+                      model_dir=model_dir,
+                      model_name=model_name,
+                      batch_size=batch_size,
+                      learning_rate=learning_rate,
+                      num_steps=num_steps)
+
+            # Start training
+            cdn.train(input_path=data_path)
+
+
+    """
+    PREDICTION
+    Use a trained CDN model to perform prediction on a given dataset
+    This model can be trained either using training only mode or train and predict mode
+    """
+    # Prediction mode
+    if mode == "predict":
+
+        with tf.Session() as sess:
+            cdn = CDN(sess=sess,
+                      model_dir=model_dir,
+                      model_name=model_name,
+                      batch_size=batch_size,
+                      learning_rate=learning_rate,
+                      scaling=scaling_option)
+
+            # Predict ratios
+            cdn.predict(input_path=data_path, out_dir=out_dir, out_name=out_name, training_data=training_data)
+
+
+
+    """
+    TRAIN_PREDICT
+    Use a prediction dataset to select the common set of genes to use as features, then train a CDN model 
+    on these features
+    After successfull training, perform prediction using this model
+    """
+    # Input-file specific training and predictions
+    if mode == "train_predict":
+        # Get signature genes overlap
+        import scanpy.api as sc
+
+        raw_input = sc.read_h5ad(training_data)
+        sig_genes_complete = list(raw_input.var_names)
+        sig_genes = get_signature_genes(input_path=data_path, sig_genes_complete=sig_genes_complete)
+
+        # Pre-process data with new signature genes
+        preprocess_h5ad_data(raw_input_path=training_data,
+                             scaling_option=scaling_option,
+                             processed_path=processed_path,
+                             group_small=True,
+                             signature_genes=True,
+                             alt_sig_genes=sig_genes)
+
+        # Perform training with new data
+        with tf.Session() as sess:
+            cdn = CDN(sess=sess,
+                      model_dir=model_dir,
+                      model_name=model_name,
+                      batch_size=batch_size,
+                      learning_rate=learning_rate,
+                      num_steps=num_steps)
+            # Start training
+            cdn.train(input_path=processed_path)
+
+        # Perform prediction
+        tf.reset_default_graph()
+        with tf.Session() as sess:
+            cdn = CDN(sess=sess,
+                      model_dir=model_dir,
+                      model_name=model_name,
+                      batch_size=batch_size,
+                      learning_rate=learning_rate,
+                      scaling=scaling_option)
+            cdn.sig_genes=sig_genes
+
+            # Predict ratios
+            cdn.predict(input_path=data_path, out_dir=out_dir, out_name=out_name, training_data=training_data)
+
+
+    # Pre-processing mode
+    """
+    PREPROCESS
+    Use built-in functionality to preprocess a dataset (h5ad) format into
+    correct scale
+    """
+    if mode == "process":
+        preprocess_h5ad_data(raw_input_path=data_path,
+                             scaling_option=scaling_option,
+                             processed_path=processed_path,
+                             group_small=True,
+                             signature_genes=False)
+
--- model/functions.py
+++ model/functions.py
@@ -9,82 +9,6 @@ from sklearn import preprocessing as pp
 import pandas as pd
 
 
-def squeeze_fractions(features):
-    """
-    Custom activation functions that turns the logits into proportions
-    that add up to 1 and are all positive
-    :param features:
-    :return:
-    """
-    abs_features = tf.abs(features)
-    max = tf.reduce_sum(abs_features, axis=0)
-    res = tf.divide(abs_features, max)
-    return res
-
-
-def _add_noise(x, y):
-    """
-    Add random noise to the sample x
-    :param x: expression values
-    :param y: ratios (not changed)
-    :return: (x_noise, y)
-    """
-    # add random noise
-    noise = tf.random_normal(shape=tf.shape(x), mean=0.0, stddev=0.2, dtype=tf.float32)
-    x = x + noise
-    # Scale again to [0,1] range
-    mms = pp.MinMaxScaler(feature_range=(0, 1), copy=True)
-    x = mms.fit_transform(x.T).T
-    return x, y
-
-
-def load_h5ad_file(input_path, batch_size, add_noise=False, datasets=['data8k', 'data6k', 'data68k', 'donorB', 'donorA', 'donorC', 'GSE65133']):
-    """
-    Load input data from a h5ad file and divide into training and test set
-    :param input_path: path to h5ad file
-    :param batch_size: batch size to use for training
-    :param datasets: a list of datasets to extract from the file
-    :return: Dataset object
-    """
-    raw_input = sc.read_h5ad(input_path)
-    test_input = raw_input.copy()
-    print(raw_input.shape)
-
-
-    # divide dataset in train and test data
-    all_ds = collections.Counter(raw_input.obs['ds'])
-    for ds in all_ds:
-        if ds in datasets:
-            test_input = test_input[test_input.obs['ds'] != ds].copy()
-        else:
-            raw_input = raw_input[raw_input.obs['ds'] != ds].copy()
-
-    # Create training dataset
-    ratios = [raw_input.obs[ctype] for ctype in raw_input.uns['cell_types']]
-    x_data = raw_input.X.astype(np.float32)
-    y_data = np.array(ratios, dtype=np.float32).transpose()
-    # create placeholders
-    x_data_ph = tf.placeholder(x_data.dtype, x_data.shape)
-    y_data_ph = tf.placeholder(y_data.dtype, y_data.shape)
-    data = tf.data.Dataset.from_tensor_slices((x_data_ph, y_data_ph))
-    # Add noise
-    if add_noise:
-        data = data.map(_add_noise)
-    data_train = data.shuffle(1000).repeat().batch(batch_size=batch_size)
-
-    # Create test dataset
-    ratios = [test_input.obs[ctype] for ctype in test_input.uns['cell_types']]
-    x_test = test_input.X.astype(np.float32)
-    y_test = np.array(ratios, dtype=np.float32).transpose()
-    x_test_ph = tf.placeholder(x_test.dtype, x_test.shape)
-    y_test_ph = tf.placeholder(y_test.dtype, y_test.shape)
-    data_test = tf.data.Dataset.from_tensor_slices((x_test_ph, y_test_ph))
-    data_test = data_test.batch(batch_size=test_input.shape[0])
-    # Extract info
-    labels = raw_input.uns['cell_types']
-
-    return data_train, (x_data_ph, y_data_ph), data_test, (x_test_ph, x_data_ph), (x_data, y_data), (x_test, y_test), labels
-
 def dummy_labels(m, labels):
     """
     Create dummy labels needed for building the graph correctly
@@ -94,32 +18,6 @@ def dummy_labels(m, labels):
     """
     n_l = len(labels)
     return np.zeros((m, n_l), dtype="float32")
-
-
-def load_prediction_file(input_path, sig_genes, labels, scaling=None):
-    """
-    Load a file to perform prediction on it
-    :param input_path: path to input file
-    :param sig_genes: the signature genes to use
-    :param scaling: which scaling to perform
-    :return: Dataset object
-    """
-    # Load data
-    data = pd.read_table(input_path, sep="\t", index_col=0)
-    sample_names = list(data.columns)
-    data = data.loc[sig_genes]
-    data = data.T
-    data = data.astype(np.float32)
-    m = data.shape[0]
-    y_dummy = dummy_labels(m, labels)
-    # Scaling
-    if not scaling == None:
-        data = sample_scaling(data, scaling_option=scaling)
-
-    # Create Dataset object
-    dataset = tf.data.Dataset.from_tensor_slices((data, y_dummy))
-    dataset = dataset.batch(batch_size=m)
-    return dataset, m, sample_names
 
 def sample_scaling(x, scaling_option):
     """
@@ -138,57 +36,6 @@ def sample_scaling(x, scaling_option):
 
         # it scales features so transpose is needed
         x = mms.fit_transform(x.T).T
-
-    elif scaling_option == "min_max":
-        # Normalize data
-        mms = pp.MinMaxScaler(feature_range=(0, 1), copy=True)
-
-        # it scales features so transpose is needed
-        x = mms.fit_transform(x.T).T
-
-    elif scaling_option == "log_robust":
-        x = np.log2(x + 1)
-        rs = pp.RobustScaler(copy=True)
-        x = rs.fit_transform(x.T).T
-
-    elif scaling_option == "robust":
-        rs = pp.RobustScaler(copy=True)
-        x = rs.fit_transform(x.T).T
-
-    elif scaling_option == "quantile":
-        qt = pp.QuantileTransformer()
-        x = qt.fit_transform(x.T).T
-
-    elif scaling_option == "log_quantile":
-        x = np.log2(x + 1)
-        qt = pp.QuantileTransformer()
-        x = qt.fit_transform(x.T).T
-
-    elif scaling_option == "quantile_gauss":
-        qt = pp.QuantileTransformer(output_distribution="normal")
-        x = qt.fit_transform(x.T).T
-
-    elif scaling_option == "feat_log_min_max":
-        x = np.log2(x + 1)
-        mms = pp.MinMaxScaler(feature_range=(0,1))
-        x = mms.fit_transform(x)
-
-    elif scaling_option == "feat_quant_gauss":
-        qt = pp.QuantileTransformer(output_distribution="normal")
-        x = qt.fit_transform(x)
-
-    elif scaling_option == "power_transform":
-        pt = pp.PowerTransformer()
-        x = pt.fit_transform(x.T).T
-
-    elif scaling_option == "lsn":
-        lib_size = 1000
-        total = x.sum(axis=1)
-        norm_factors = np.divide(lib_size, total)
-        nf_array = np.array([norm_factors]*x.shape[1]).T
-        print(nf_array.shape)
-        x = np.multiply(x, nf_array)
-
 
     return x
 
