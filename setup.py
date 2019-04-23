cf148854f4f84d76d1d22090392706fd5a7918c4
KevinMenden <kevin.menden@t-online.de>
2019-04-23 13:01:33 +0200

    Added gitignore, Manifest; fixed setup.py

--- setup.py
+++ setup.py
@@ -7,24 +7,31 @@ version = '1.0.0'
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name = 'cdn',
+    version = version,
     description = "Cell type deconvolution using single cell data",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     keywords = ['bioinformatics', 'deep learning', 'machine learning', 'single cell sequencing', 'deconvolution'],
     author = 'Kevin Menden',
     author_email = 'kevin.menden@t-online.de',
-    scripts = ['cdn'],
+    license = license,
+    scripts = ['scripts/cdn'],
     packages = find_packages(),
+    include_package_data = True,
     install_requires = [
-        'pandas',
-        'numpy',
+        'pandas==0.21',
+        'numpy==1.14.5',
         'scikit-learn',
         'scipy',
-        'tensorflow=1.10.0',
         'seaborn',
-        'scanpy=1.2.2',
+        'tensorflow==1.10.0',
+        'matplotlib',
+        'scanpy==1.2.2',
         'tqdm',
         'click'
     ]
